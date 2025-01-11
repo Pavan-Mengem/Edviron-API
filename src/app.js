3b2966b5d898d4e301627f3b3ccbb07e3a3524bf
Pavan-Mengem <167200740+Pavan-Mengem@users.noreply.github.com>
2025-01-11 17:10:21 +0530

    misc change

--- src/app.js
+++ src/app.js
@@ -4,10 +4,8 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+// allow all cors requests
+app.use(cors({origin: true, credentials: true}))
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
