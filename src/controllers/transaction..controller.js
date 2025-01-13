2e864cef0de4fa0cc5394760805530d3a7b070c5
Pavan-Mengem <167200740+Pavan-Mengem@users.noreply.github.com>
2025-01-13 08:14:49 +0530

    Enhance transaction retrieval to include status and payment details

--- src/controllers/transaction..controller.js
+++ src/controllers/transaction..controller.js
@@ -33,7 +33,20 @@ export  const GetTransctionBySchool = async (req, res) => {
         return res.status(400).json({ message: "School_id is required" });
       }
       const transactions = await Transaction.find({ school_id });
-      res.status(200).json(transactions);
+      const transactionStatuses = await TransactionStatus.find();
+
+      const combinedData = transactions.map(transaction => {
+        const status = transactionStatuses.find(status => status.custom_order_id === transaction.custom_order_id);
+        return {
+          ...transaction.toObject(),
+          status: status ? status.status : null,
+          payment_method: status ? status.payment_method : null,
+          gateway: status ? status.gateway : null,
+          bank_reference: status ? status.bank_reference : null,
+        };
+      });
+  
+      res.status(200).json(combinedData);
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
